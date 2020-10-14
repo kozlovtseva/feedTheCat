ed3cf8e6b416fcd1295397b4230fdb26ed8ae889
Alina <ka-front@fulogy.com>
2020-10-14 16:33:46 +0300

    add list data

---
+++ src/data.js
@@ -0,0 +1,38 @@
+export const catsFoodList = [
+    {
+        id: 0,
+        name: "Нямушка",
+        subname: "с фуа-гра",
+        type: "Сказочное заморское яство",
+        promo: {
+            portion: 10,
+            mouses: 1,
+        },
+        weight: 0.5,
+        description: "Печень утки разварная с артишоками.",
+    },
+    {
+        id: 1,
+        name: "Нямушка",
+        subname: "с рыбой",
+        type: "Сказочное заморское яство",
+        promo: {
+            portion: 10,
+            mouses: 1,
+        },
+        weight: 0.5,
+        description: "Головы щучьи с чесноком да свежайшая сёмгушка.",
+    },
+    {
+        id: 2,
+        name: "Нямушка",
+        subname: "с курой",
+        type: "Сказочное заморское яство",
+        promo: {
+            portion: 10,
+            mouses: 1,
+        },
+        weight: 0.5,
+        description: "Филе из цыплят с трюфелями в бульоне.",
+    },
+];
