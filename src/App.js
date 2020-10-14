3b720c5638116d4a17ac717d30ba62c6470301f4
Alina <ka-front@fulogy.com>
2020-10-14 16:18:43 +0300

    layout styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Layout from "./layouts/Layout";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <Layout />
+        </div>
+    );
 }
 
 export default App;
