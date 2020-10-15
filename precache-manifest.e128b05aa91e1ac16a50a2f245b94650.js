0b14fcecf28b53a6bdb5354beaacc048f6372138
Alina <ka-front@fulogy.com>
2020-10-15 16:07:27 +0300

    Updates

---
+++ precache-manifest.e128b05aa91e1ac16a50a2f245b94650.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80e11ca432f27cd7e973bf9597e09b20",
+    "url": "/feedTheCat/index.html"
+  },
+  {
+    "revision": "5c0a3542381348aacf77",
+    "url": "/feedTheCat/static/css/main.f1aa9646.chunk.css"
+  },
+  {
+    "revision": "d5c0694367ce6bc4e768",
+    "url": "/feedTheCat/static/js/2.35be728c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/feedTheCat/static/js/2.35be728c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c0a3542381348aacf77",
+    "url": "/feedTheCat/static/js/main.34ba571d.chunk.js"
+  },
+  {
+    "revision": "92d0313d00dc2d2f90a3",
+    "url": "/feedTheCat/static/js/runtime-main.9e91558e.js"
+  },
+  {
+    "revision": "44bde8744ccc0de7995a23b0e82dd281",
+    "url": "/feedTheCat/static/media/Exo2.0-Thin.44bde874.otf"
+  },
+  {
+    "revision": "f1bfed4b6abfe428fc39710877602c31",
+    "url": "/feedTheCat/static/media/cat.f1bfed4b.png"
+  }
+]);
